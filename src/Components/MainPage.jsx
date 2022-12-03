f152c38cf2709a5c238e6a212e34cb2896b97277
David <callmelmao@gmail.com>
2022-12-03 17:19:11 +0000

    added 10 live stocks

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -2,12 +2,101 @@ import Diagram from "./Diagram"
 import Fall from "./Fall"
 import Rise from "./Rise"
 import Stocks from "./Stocks"
+import * as finnhub from 'finnhub'
+import { useEffect, useState } from 'react';
+
+
 
 function MainPage() {
+    const [liveApple, setLiveApple] = useState(0)
+    const [liveTesla, setLiveTesla] = useState(0)
+    const [liveNike, setLiveNike] = useState(0)
+    const [liveMicrosoft, setLiveMicrosoft] = useState(0)
+    const [liveMeta, setLiveMeta] = useState(0)
+    const [liveDisney, setLiveDisney] = useState(0)
+    const [liveAmazon, setLiveAmazon] = useState(0)
+    const [liveNvidia, setLiveNvidia] = useState(0)
+    const [liveStarbucks, setLiveStarbucks] = useState(0)
+    const [liveNetflix, setLiveNetflix] = useState(0)
+
+
+    const [appleCandles24, setAppleCandles24] = useState([]);
+    const [high, setHigh] = useState(0);
+    const [low, setLow] = useState(0);
+  
+    useEffect(() => {
+        const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+        api_key.apiKey = "cdukjcaad3ib3oqti880cdukjcaad3ib3oqti88g"
+        const finnhubClient = new finnhub.DefaultApi()
+
+        finnhubClient.quote("AAPL", (error, data, response) => setLiveApple(data.c))
+        finnhubClient.quote("TSLA", (error, data, response) => setLiveTesla(data.c))
+        finnhubClient.quote("NKE", (error, data, response) => setLiveNike(data.c))
+        finnhubClient.quote("MSFT", (error, data, response) => setLiveMicrosoft(data.c))
+        finnhubClient.quote("META", (error, data, response) => setLiveMeta(data.c))
+        finnhubClient.quote("DIS", (error, data, response) => setLiveDisney(data.c))
+        finnhubClient.quote("AMZN", (error, data, response) => setLiveAmazon(data.c))
+        finnhubClient.quote("NVDA", (error, data, response) => setLiveNvidia(data.c))
+        finnhubClient.quote("SBUX", (error, data, response) => setLiveStarbucks(data.c))
+        finnhubClient.quote("NFLX", (error, data, response) => setLiveNetflix(data.c))
+
+
+    
+    //   setTimeout(()=>{
+    //     finnhubClient.quote("AAPL", (error, data, response) => {
+    //       if(error) return console.error(error) 
+    //       console.log(data, response)
+    //       setAppleStock(data)
+    //     });
+    //   },5000)
+  
+      // get news for apple between two time periods
+      // finnhubClient.companyNews("AAPL", "2022-01-01", "2022-11-10", (error, data, response) => {
+      //   if(error) return console.error(error)
+      //   console.log(data, response)
+      // });
+  
+      //initial price data for last 24 hours
+    //   finnhubClient.stockCandles("AAPL", "1", parseInt((Date.now() - 1000*60*60*24)/1000), parseInt(Date.now() / 1000), (error, data, response) => {
+    //     if(error) return console.error(error)
+    //     console.log(data, response)
+    //     if(data.s === "ok") setAppleCandles24(data.c)
+    //   });
+
+    //   // continue to get apple price every 1 minute
+    //   setInterval(()=>{
+    //     finnhubClient.stockCandles("AAPL", "1", parseInt(Date.now()/1000)-60, parseInt(Date.now() / 1000), (error, data, response) => {
+    //       if(error) return console.error(error.message)
+    //       console.log(data, response)
+    //       if(data.s === "ok") setAppleCandles24(prev => [...prev, data.c[0]])
+    //     });
+    //   }, 1000*60*1)
+    }, [])
+  
+    // useEffect(()=> {
+    //   console.log("apple update", appleCandles24)
+    //   if(appleCandles24.length > 0) {
+    //     setHigh(Math.max(...appleCandles24))
+    //     setLow(Math.min(...appleCandles24))
+    //   }
+    // }, [appleCandles24])
+
+
     return (
         <div className="MainPage flex justify-between items-start h-[85vh] px-8 py-4">
             <div className="w-3/12 h-full border-2 border-black px-4 py-4">
-                <Stocks />
+                <Stocks 
+                    liveApple={liveApple}
+                    liveTesla={liveTesla}
+                    liveNike={liveNike}
+                    liveMicrosoft={liveMicrosoft}
+                    liveMeta={liveMeta}
+                    liveDisney={liveDisney}
+                    liveAmazon={liveAmazon}
+                    liveNvidia={liveNvidia}
+                    liveStarbucks={liveStarbucks}
+                    liveNetflix={liveNetflix}
+                />
             </div>
             <div className="flex justify-between items-start flex-col w-8/12 h-full border-2 border-black gap-4 px-4 py-4">
                 <Diagram />
--- src/Components/Stocks.jsx
+++ src/Components/Stocks.jsx
@@ -1,15 +1,47 @@
-function Stocks() {
+import { useState } from "react"
+
+function Stocks({ liveApple, liveTesla, liveNike, liveMicrosoft, liveMeta, liveDisney, liveAmazon, liveNvidia, liveStarbucks, liveNetflix } ) {
+
+    const oftenStocks = [
+        { name: 'Apple', shortname: 'AAPL', price: liveApple },
+        { name: 'Tesla', shortname: 'TSLA', price: liveTesla },
+        { name: 'Nike', shortname: 'NKE', price: liveNike },
+        { name: 'Microsoft', shortname: 'MSFT', price: liveMicrosoft },
+        { name: 'Meta', shortname: 'META', price: liveMeta },
+        { name: 'Disney', shortname: 'DIS', price: liveDisney },
+        { name: 'Amazon', shortname: 'AMZN', price: liveAmazon },
+        { name: 'Nvidia', shortname: 'NVDA', price: liveNvidia },
+        { name: 'Starbucks', shortname: 'SBUX', price: liveStarbucks },
+        { name: 'Netflix', shortname: 'NFLX', price: liveNetflix }
+    ]
+
     return (
         <div className="Stocks">
             <h3 className="text-xl text-center">Often looking for</h3>
-            <div className="list flex justify-between items-center">
+            <div className="flex justify-between items-center flex-col">
+            {oftenStocks.map(stock => (
+                <div className="flex justify-between items-center w-full">
+                    <div className="flex flex-col w-4/12">
+                        <span>{stock.shortname}</span>
+                        <span>{stock.name}</span>
+                    </div>
+                    <div className="w-4/12 text-center">chart</div>
+                    <div className="flex flex-col w-4/12 text-right">
+                        <span>{stock.price}</span>
+                        <span>daily change</span>
+                    </div>
+                </div>
+            ))}
+            </div>
+            
+            {/*<div className="list flex justify-between items-center">
                 <div className="name flex flex-col">
                     <span>short name</span>
                     <span>full name</span>
                 </div>
                 <div>chart</div>
                 <div>price</div>
-            </div>
+            </div>*/}
         </div>
     )
 }
